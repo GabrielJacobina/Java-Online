1de56840d02076c711234e867bc41516192fa34f
GabrielJacobina <sgabrieljacobina@gmail.com>
2021-02-01 21:40:36 -0300

    Meta caracteres

--- src/br/com/abc/javacore/Rexpressoesregulares/test/ExpressoesRegularesTest.java
+++ src/br/com/abc/javacore/Rexpressoesregulares/test/ExpressoesRegularesTest.java
@@ -5,16 +5,23 @@ import java.util.regex.Pattern;
 
 public class ExpressoesRegularesTest {
     public static void main(String[] args) {
-        String regex = "aba";
-        String texto = "abababa";
+        // \d - todos os digitos
+        // \D - tudo o que não for digito
+        // \s - espaços em branco \t \n \f \r
+        // \S - caracter que não é branco
+        // \w - caracteres de palavras a-z A-Z, digitos e _
+        // \W - tudo o que não for caractere de palavra
+        // []
+        String regex = "0[xX][0-9a-fA-F]";
+        String texto = "12 0x 0X 0xFFABC 0x10G 0x1";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(texto);
         System.out.println("texto:  "+texto);
-        System.out.println("indice: 012456789");
+        System.out.println("indice: 012345678901234567");
         System.out.println("expressao: "+matcher.pattern());
         System.out.println("posicoes encontradas");
         while (matcher.find()) {
-            System.out.print(matcher.start() + " ");
+            System.out.println(matcher.start() + " " + matcher.group());
         }
     }
 }
