2f0323ad3ad7c9237a2914e09d3db6e3be2f6c2a
GabrielJacobina <sgabrieljacobina@gmail.com>
2021-02-03 23:56:54 -0300

    Ordenação de listas e arrays com Comparator

--- src/br/com/abc/javacore/Zcolecoes/test/SortProdutoTest.java
+++ src/br/com/abc/javacore/Zcolecoes/test/SortProdutoTest.java
@@ -2,23 +2,41 @@ package br.com.abc.javacore.Zcolecoes.test;
 
 import br.com.abc.javacore.Zcolecoes.classes.Produto;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+
+class ProdutoNomeComparator implements Comparator<Produto>{
+
+    @Override
+    public int compare(Produto produto, Produto t1) {
+        return produto.getNome().compareTo(t1.getNome());
+    }
+}
 
 public class SortProdutoTest {
     public static void main(String[] args) {
         List<Produto> produtos = new ArrayList<>();
+        Produto[] produtosArray = new Produto[4];
         Produto produto1 = new Produto("123", "Laptop Lenovo", 2000.0);
         Produto produto2 = new Produto("321", "Picanha", 26.4);
         Produto produto3 = new Produto("879", "Teclado Razer", 1000.0);
         Produto produto4 = new Produto("012", "Samsung galaxy S7 64 Gb", 3250.5);
+
         produtos.add(produto1);
         produtos.add(produto2);
         produtos.add(produto3);
         produtos.add(produto4);
-        Collections.sort(produtos);
-        for (Produto produto : produtos){
+
+        produtosArray[0] = produto1;
+        produtosArray[1] = produto2;
+        produtosArray[2] = produto3;
+        produtosArray[3] = produto4;
+
+        Collections.sort(produtos, new ProdutoNomeComparator());
+//        for (Produto produto : produtos){
+//            System.out.println(produto);
+//        }
+        Arrays.sort(produtosArray, new ProdutoNomeComparator());
+        for (Produto produto : produtosArray){
             System.out.println(produto);
         }
     }
