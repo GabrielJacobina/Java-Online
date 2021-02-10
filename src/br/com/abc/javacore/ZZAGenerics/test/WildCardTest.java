a3a5be3db63083dd58ebc6586d642d47c6da79cc
GabrielJacobina <sgabrieljacobina@gmail.com>
2021-02-09 23:58:02 -0300

    WildCard

---
+++ src/br/com/abc/javacore/ZZAGenerics/test/WildCardTest.java
@@ -0,0 +1,71 @@
+package br.com.abc.javacore.ZZAGenerics.test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+abstract class Animal {
+    public abstract void consulta();
+}
+
+class Cachorro extends Animal implements Comparable {
+
+    @Override
+    public void consulta() {
+        System.out.println("Consultando cachorro");
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+}
+
+class Gato extends Animal {
+
+    @Override
+    public void consulta() {
+        System.out.println("Consultando gato");
+    }
+}
+
+public class WildCardTest {
+    public static void main(String[] args) {
+        Cachorro[] cachorros = {new Cachorro(), new Cachorro()};
+        Gato[] gatos = {new Gato(), new Gato()};
+//        consultarAnimais(cachorros);
+//        consultarAnimais(gatos);
+        List<Cachorro> cachorroList = new ArrayList<>();
+        cachorroList.add(new Cachorro());
+        List<Gato> gatoList = new ArrayList<>();
+        gatoList.add(new Gato());
+        consultarAnimaisList(cachorroList);
+        consultarAnimaisList(gatoList);
+        ordenarLista(cachorroList);
+    }
+
+    public static void consultarAnimais(Animal[] animais) {
+        for (Animal animal : animais) {
+            animal.consulta();
+        }
+//        animais[1] = new Gato();
+    }
+
+    //type erasure | ?
+    public static void consultarAnimaisList(List<? extends Animal> animais) {
+        for (Animal animal : animais) {
+            animal.consulta();
+        }
+    }
+
+    public static void consultarCachorrosList(List<? super Cachorro> cachorroList) {
+        Cachorro c1 = new Cachorro();
+        Animal c2 = new Cachorro();
+        Object o = new Cachorro();
+        cachorroList.add(new Cachorro());
+    }
+
+    public static void ordenarLista(List<? extends Comparable> lista) {
+        Collections.sort(lista);
+    }
+}
