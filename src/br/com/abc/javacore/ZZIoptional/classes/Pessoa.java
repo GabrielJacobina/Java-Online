e15dff5a36e9a798459e31d62d2f3a295f729689
GabrielJacobina <sgabrieljacobina@gmail.com>
2021-03-04 12:02:05 -0300

    Optional with try catch

--- src/br/com/abc/javacore/ZZIoptional/classes/Pessoa.java
+++ src/br/com/abc/javacore/ZZIoptional/classes/Pessoa.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 public class Pessoa {
     private Carro carro;
     private String nome;
+    private int idade;
 
     public Pessoa(Carro carro, String nome) {
         this.carro = carro;
@@ -13,6 +14,14 @@ public class Pessoa {
 
     public Pessoa(String nome) {
         this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
     }
 
     public Optional<Carro> getCarro() {
--- src/br/com/abc/javacore/ZZIoptional/test/OptionalTest2.java
+++ src/br/com/abc/javacore/ZZIoptional/test/OptionalTest2.java
@@ -4,14 +4,59 @@ import br.com.abc.javacore.ZZIoptional.classes.Carro;
 import br.com.abc.javacore.ZZIoptional.classes.Pessoa;
 import br.com.abc.javacore.ZZIoptional.classes.Seguradora;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class OptionalTest2 {
     public static void main(String[] args) {
         Seguradora seguradora = new Seguradora("DevDojo Seguros");
-        Carro carro = new Carro(null, "Audi");
+        Carro carro = new Carro(seguradora, "Audi");
         Pessoa p = new Pessoa(carro, "Carlos");
         obterNomeSeguradora(Optional.ofNullable(p));
+        checarNomeSeguradora(seguradora);
+        checarNomeSeguradoraOptional(seguradora);
+        p.setIdade(20);
+        System.out.println(obterNomeSeguradoraComIdade(p, 18));
+        Map<String, String> map = new HashMap<>();
+        System.out.println(Optional.ofNullable(map.get("kaka")));
+        stringToInt("A");
+    }
+
+    /***
+     *
+     * @param numero
+     * @return
+     */
+    private static Optional<Integer> stringToInt(String numero) {
+        try {
+            return Optional.of(Integer.parseInt("A"));
+        } catch (NumberFormatException e) {
+//            e.printStackTrace();
+            return Optional.empty();
+        }
+    }
+
+    private static void checarNomeSeguradora(Seguradora seguradora) {
+        if (seguradora != null && seguradora.getNome().equals("DevDojo Seguros")) {
+            System.out.println("é o devdojo");
+        }
+    }
+
+    private static void checarNomeSeguradoraOptional(Seguradora seguradora) {
+        Optional.ofNullable(seguradora).filter(s -> s.getNome().equals("DevDojo Seguros"))
+                .ifPresent(x -> {
+                    System.out.println("é o devdojo");
+                });
+    }
+
+    private static String obterNomeSeguradoraComIdade(Pessoa p, int idadeMinima) {
+        return Optional.ofNullable(p)
+                .filter(pessoa -> pessoa.getIdade() >= idadeMinima)
+                .flatMap(Pessoa::getCarro)
+                .flatMap(Carro::getSeguradora)
+                .map(Seguradora::getNome)
+                .orElse("Não existe seguradora ou a idade minima não foi atingida");
     }
 
     public static String obterNomeSeguradora(Optional<Pessoa> p) {
